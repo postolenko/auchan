cee5429708e43b889f59e8fdfd02a2d25f6ebe99
postolenko <denis.postolenko@yandex.ua>
2016-07-27 04:22:11 +0300

    Auchan

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+    $(document).scroll(function() {
+
+
+
+    });
+
+    $(window).resize(function() {
+
+
+
+    });
+
+
+
+
+// -----------------
+
+    $(".scroll-to-top").click(function () {
+
+        $("body,html").animate({
+
+            scrollTop: 0
+
+        }, 1000);
+
+        return false;
+
+    });
+
+
+    function getScrollToTopBtn() {
+
+
+        if ($(window).scrollTop() > $(".header-bg").height() ) {
+
+            $(".scroll-to-top").fadeIn();
+
+        } else {
+
+            $(".scroll-to-top").fadeOut();
+
+        }
+
+    }
+
+
+
+});
