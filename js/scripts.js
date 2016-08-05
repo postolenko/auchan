42154dec7a45cd4a8f6b840a080d64eb2e961c9a
postolenko <denis.postolenko@yandex.ua>
2016-08-05 05:24:58 +0300

    Auchan

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,59 @@ $(document).ready(function() {
 
     $(".header-page-right-col").css({"min-height" : $(".header-page-nav-list").height() + "px"});
 
+
+
+
+
+    var indexModalBox;
+    var topCoorDefault;
+    var topCoor;
+
+    $(".show-popup").click(function() {
+
+        indexModalBox = $(".show-popup").index(this);
+
+        if( !$(".modal-box:eq("+ indexModalBox +")").hasClass("show-modal-block") ) {
+
+            $(".modal-box:eq("+ indexModalBox +")").addClass("show-modal-block");
+            
+
+            topCoor = parseInt ( $(".modal-box:eq("+ indexModalBox +").show-modal-block").css("top") );
+
+
+            $(".modal-box:eq("+ indexModalBox +").show-modal-block").css({"top" : topCoor - 10 + "px"});
+
+            setTimeout(function() {
+
+                $(".modal-box:eq("+ indexModalBox +").show-modal-block").animate({
+                                                                            "opacity" : 1,
+                                                                            "top" : topCoor + "px"
+                                                                        }, 500);
+
+            }, 100);
+
+
+        } else {
+
+
+            $(".modal-box:eq("+ indexModalBox +").show-modal-block").animate({
+                                                                            "opacity" : 0,
+                                                                            "top" : topCoor - 10 + "px"
+                                                                        }, 500);
+
+            setTimeout(function() {
+
+                $(".modal-box:eq("+ indexModalBox +").show-modal-block").css({"top" : ""});
+                $(".modal-box:eq("+ indexModalBox +")").removeClass("show-modal-block");
+
+            }, 510);
+
+        }
+        
+
+    });
+
+
     // function getPwidth() {
 
     //     for( countCellCicklum = 0; countCellCicklum <= countTableCell - 1; countCellCicklum++ ) {
